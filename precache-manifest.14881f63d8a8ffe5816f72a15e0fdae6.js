a21acef46894eb4a8494b92a9695695fbdd561de
Daniel <staszekdaniel5@gmail.com>
2019-08-26 12:08:29 +0200

    Updates

---
+++ precache-manifest.14881f63d8a8ffe5816f72a15e0fdae6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9803d3ca3f0a9676102c81e80ea376e3",
+    "url": "/Memory-game/index.html"
+  },
+  {
+    "revision": "7b5a9db2eced1a3acdd1",
+    "url": "/Memory-game/static/css/main.4bc7d001.chunk.css"
+  },
+  {
+    "revision": "1c4f2e898c7f34f90ccc",
+    "url": "/Memory-game/static/js/2.443ff43c.chunk.js"
+  },
+  {
+    "revision": "7b5a9db2eced1a3acdd1",
+    "url": "/Memory-game/static/js/main.74847453.chunk.js"
+  },
+  {
+    "revision": "25e9ce634d892d176135",
+    "url": "/Memory-game/static/js/runtime~main.679001c3.js"
+  },
+  {
+    "revision": "e224f5ed261735072b00bdb3bae20cbd",
+    "url": "/Memory-game/static/media/angular2.e224f5ed.svg"
+  },
+  {
+    "revision": "d255e75f6f38e21bc5b17f296248339c",
+    "url": "/Memory-game/static/media/babel.d255e75f.svg"
+  },
+  {
+    "revision": "2aff8e66261ead34b0bf666d1ad10346",
+    "url": "/Memory-game/static/media/bower.2aff8e66.svg"
+  },
+  {
+    "revision": "c18a4d86ff0439f1cae0182e26036d43",
+    "url": "/Memory-game/static/media/browserify.c18a4d86.svg"
+  },
+  {
+    "revision": "ad11f1d9c91052358dfe40c0b02d2451",
+    "url": "/Memory-game/static/media/ember.ad11f1d9.svg"
+  },
+  {
+    "revision": "7835f1c5e7f781ce68eba53c4e93cd69",
+    "url": "/Memory-game/static/media/grunt.7835f1c5.svg"
+  },
+  {
+    "revision": "771551b909fd715c3a185f8e86225bb8",
+    "url": "/Memory-game/static/media/gulp.771551b9.svg"
+  },
+  {
+    "revision": "83ff7531209967d27f6f95c4549808a6",
+    "url": "/Memory-game/static/media/ionic.83ff7531.svg"
+  },
+  {
+    "revision": "2a05c00f5ab999401f4790fc9b3f395d",
+    "url": "/Memory-game/static/media/meteor.2a05c00f.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/Memory-game/static/media/nodejs.94cafb0d.svg"
+  },
+  {
+    "revision": "4ffaf2930fa525b929a5b4eff002830d",
+    "url": "/Memory-game/static/media/npm.4ffaf293.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/Memory-game/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "22efb7c22774c39c790448f62bda240d",
+    "url": "/Memory-game/static/media/vue.22efb7c2.svg"
+  },
+  {
+    "revision": "878ab38467e5d9ce11b5add119296fd9",
+    "url": "/Memory-game/static/media/yarn.878ab384.svg"
+  },
+  {
+    "revision": "55e964d36d3cb4a5cf35720c9a9a371a",
+    "url": "/Memory-game/static/media/yeoman.55e964d3.svg"
+  }
+]);
